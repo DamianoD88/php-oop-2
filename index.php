29f3a46704e6c7338b604aacbb66d59b36ed3c1d
DamianoD88 <dispe88@live.it>
2021-09-02 16:28:13 +0200

    add construct

--- index.php
+++ index.php
@@ -7,4 +7,12 @@ class Utenti {
     public $age;
     public $sconti;
 
+    function __construct($_name, $surname, $email, $age){
+        $this->name = $_name;
+        $this->surname = $_surname;
+        $this->email =$_email;
+        $this->age = $_age;
+
+    }
+
 }
